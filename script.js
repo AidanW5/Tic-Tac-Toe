c1020161be3c3466a49a2be6d86143424ad81da9
Aidan Wildhaber <awildhaber55@gmail.com>
2025-10-05 23:49:13 -0500

    Select x and o button

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const playCard = document.getElementsByClassName('play_card');
+const playCard = document.querySelector(".play_card");
+
 
 function playNow() {
-    
-    const playBtn = document.getElementById('play_now');
+    playBtn = document.getElementById("play_now");
     playBtn.remove();
+
+    const x_o = playCard.innerHTML = `
+    <p id="x">X</p>
+    <p id="o">O</p>`
+
+    const getBtn = document.querySelectorAll("p");
+
+    getBtn.forEach(element => {
+        element.onclick = function() {
+            document.querySelector(".special")?.classList.remove("special");
+            element.classList.add("special");
+        }
+    });
+
 }
